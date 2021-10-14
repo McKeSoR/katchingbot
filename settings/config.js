1a8920d3e395fb242c574412c62454a5a324558c
McKeSoR <ittopgamer@mail.ru>
2021-10-14 21:18:01 +0300

    alpha

---
+++ settings/config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+	codename: "asdasdaaaaddasd|BOT",//Кодовое имя
+	namebot: "asdasdaaaaddasd|BOT", //Имя бота
+	version: "3.1", //Версия кода
+	admin: "[direnyov|Даниил]", //2 руководитель бота
+	coder: "[direnyov|Даниил]", //Для доната
+	zam: "Не найден", //Заместитель
+	site: "В группе", //Сайт, если есть
+	group_url: "[asdasdaaaaddasd|asdasdaaaaddasd|BOT]",//12213
+}
